af7a289315626b77bdb3b4c0185f02cd28c588d9
Quang Vinh <ngoquangvinh1997tg@gmail.com>
2018-12-28 16:15:03 +0700

    adding starting project

---
+++ script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var css1 = document.querySelector("h2");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
+
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ", " 
+	+color3.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+	
+}
+
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
+
+window.addEventListener("load", setGradient);
+
+
+
+
